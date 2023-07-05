7164ade7a54dcb5aa9cf8c8d05db10141844e853
Sasquatch <benjamin.kittendorf@gmail.com>
2023-07-05 12:11:45 -0400

    Cleanup

--- proghum010723/main.rs
+++ proghum010723/main.rs
@@ -1,12 +1,6 @@
 
 const MAX_NUM: usize = 100;
 
-/// An array of function pointers (or closures) that take a number, a cache of primes, and the last computed sqrt
-/// 
-/// The Difficult tier requires a cache of primes (to avoid recomputing primes), so we need to pass it to everything 😩
-/// 
-/// `cache` is an array of tri-state `Option<bool>`s, where `None` means "not computed", `Some(true)` means "prime", and `Some(false)` means "not prime"
-/// The length is MAX_NUM+7 because we need to cache the primes up to 100, plus look-aheads for the Difficult tier
 const CHECKS: [(&'static str, fn(usize, &mut [Option<bool>; MAX_NUM+7], &mut usize) -> bool); 6] = [
 	// Easy
 	("Fizz", |i, _, _| i % 3 == 0),
@@ -34,34 +28,33 @@ fn main() {
 
 /// The difficult check is a bit more complicated, so it gets its own function
 fn difficult(i: usize, cache: &mut [Option<bool>; MAX_NUM+7], last_sqrt: &mut usize) -> bool {
-	// These lookaheads pre-populate the cache, before `i` itself is checked
-	let prepop_max = (i+1..).find(|x| x % 11 == 0 || x % 7 == 0).expect("How did this overflow??");
+	// Find the next multiple of 7 or 11
+	let next_mult = (i+1..).find(|x| x % 11 == 0 || x % 7 == 0).expect("How did this overflow??");
 
-	// `i` is prime, but none of the lookaheads are
-	check_prime(i, cache, last_sqrt) && !(i+1..prepop_max).any(|x| check_prime(x, cache, last_sqrt))
+	is_prime(i, cache, last_sqrt) && !(i+1..next_mult).any(|x| is_prime(x, cache, last_sqrt))
 }
 
-fn check_prime(i: usize, primes: &mut [Option<bool>; MAX_NUM+7], last_sqrt: &mut usize) -> bool {
+// Check if `i` is prime, cache the result, and return it
+fn is_prime(i: usize, primes: &mut [Option<bool>; MAX_NUM+7], last_sqrt: &mut usize) -> bool {
 	// increment the sqrt if it's too low
 	*last_sqrt += ((*last_sqrt+1).pow(2) <= i) as usize;
 
 	// Get the cached result, or compute it. the `filter` will skip non-prime factors
 	primes[i] = primes[i].or_else(|| Some([(2..=*last_sqrt).filter(|x| !matches!(primes[*x], Some(false))).find(|j| i % j == 0), Some(0)][(*last_sqrt == i) as usize].is_none()));
 
-	// Re-access the cache. Technically inefficient, but not a good way to do this without if statements
 	primes[i].unwrap()
 }
 
 #[cfg(test)]
 mod test {
-    use crate::{check_prime, MAX_NUM};
+    use crate::{is_prime, MAX_NUM};
 
 	#[test]
-	fn test_check_prime() {
+	fn test_is_prime() {
 		let (mut primes, mut last_sqrt) = ([None; MAX_NUM+7], 1);
 		let mut confirmed = Vec::new();
 		for i in 1..=100 {
-			if check_prime(i, &mut primes, &mut last_sqrt) {
+			if is_prime(i, &mut primes, &mut last_sqrt) {
 				confirmed.push(i);
 			}
 		}
